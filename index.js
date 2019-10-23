dc3b763ed2485bca18bd0201417db726b412324f
rednaxela700 <ohleg@windowslive.com>
2019-10-23 13:26:01 +0200

    deleted part of previous solution, adding students using function constructor now

--- index.js
+++ index.js
@@ -16,25 +16,33 @@ const ultimateStudentES6 = {
 
 };
 
-//creating list of students
-const properStudent = Object.create((ultimateStudentES6));
-properStudent.setStudentData('Jedrzej', 'Metalowa 666', 12323, 'Medical Student');
-properStudent.displayInfo();
-const properStudent2 = Object.create((ultimateStudentES6));
-properStudent2.setStudentData('Andrzej', 'Rockowa 555', 8850, 'Engineer');
+//creating list of students USING CONSTRUCTOR FUNCTION
 
-const properStudent3 = Object.create((ultimateStudentES6));
+function student(name, address, idNumber, profile) {
+    this.name = name;
+    this.address = address;
+    this.idNumber = idNumber;
+    this.changeId = function (newId) {
+        this.idNumber = newId;
+    };
+    this.profile = profile;
+}
+
+//creating student using constructor above
+const student1 = new student('Adam', 'Konwaliowa 2', 23232, 'Medical Diagnosis' );
+const student2 = new student('Pawel', 'Wesola 25', '00000', 'Psychology' );
+const student3 = new student('Gawel', 'zxc 222', '111', 'ASD' );
+const student4 = new student('Michal', 'Wesola 25', '00000', 'WQWWWW' );
 
 
-properStudent3.setStudentData('DzejDzej', 'Jazzowa 1358', 12323, 'SSS');
 
-
+student3.changeId(77777);
 //list arr
 const studentsList = [];
-
+studentsList.push(student1, student2, student3, student4);
 
 let outputHtml = '';
-studentsList.push(properStudent, properStudent2, properStudent3);
+studentsList.push();
 console.log(studentsList);
 const htmlAnchor = document.querySelector('.student__container');
 
