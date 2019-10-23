bb19fb0bcccc240b0dbd1a7a52154c7f769dda50
rednaxela700 <ohleg@windowslive.com>
2019-10-23 11:38:56 +0200

    added setStudentData method, and defined 4 students, which are being kept in an array

--- index.js
+++ index.js
@@ -31,32 +31,39 @@ const studentES6 = {
 
 const ultimateStudentES6 = {
     name: '',
-    setName: function (arg) {
-        this.name = arg
-    },
     address: '',
-    setAddress: function (arg) {
-        this.address = arg.toString()
-    },
     idNumber: '',
-    setIdNumber: function (arg) {
-        this.idNumber = arg
-    },
     profile: '',
-    setProfile: function (arg) {
-        this.profile = arg
+    setStudentData: function (name, address, idNumber, profile) {
+        this.name = name;
+        this.address = address;
+        this.idNumber= idNumber;
+        this.profile= profile;
     },
-
     displayInfo: function () {
         console.log(`${this.name}\n${this.address}\n${this.idNumber}\n${this.profile}`);
     }
 
 
 };
+const properStudent = Object.create((ultimateStudentES6));
+properStudent.setStudentData('Jedrzej', 'Metalowa 666', 12323, 'Medical Student');
+properStudent.displayInfo();
+const properStudent2 = Object.create((ultimateStudentES6));
+properStudent2.setStudentData('Andrzej', 'Rockowa 555', 8850, 'Engineer');
 
-const firstStudent = Object.create(ultimateStudentES6);
-firstStudent.setName('Michal');
-firstStudent.setAddress('Ulica 1');
-firstStudent.setIdNumber(12323);
-firstStudent.setProfile('Medical Student');
-console.log(firstStudent);
+// const firstStudent = Object.create(ultimateStudentES6);
+// firstStudent.setName('Michal');
+// firstStudent.setAddress('Ulica 1');
+// firstStudent.setIdNumber(12323);
+// firstStudent.setProfile('Medical Student');
+// console.log(firstStudent);
+const properStudent3 = Object.create((ultimateStudentES6));
+
+
+properStudent3.setStudentData('DzejDzej', 'Jazzowa 1358', 12323, 'SSS');
+
+const studentsList = []
+
+studentsList.push(properStudent, properStudent2, properStudent3);
+console.log(studentsList);
