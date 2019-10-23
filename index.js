c1d5607dec31f37652084e2fa60158001d63fe60
rednaxela700 <ohleg@windowslive.com>
2019-10-23 12:56:16 +0200

    added a function generating student__item cells

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-var studentES5 = {
-    name: 'Andrzej Wajda',
-    address: 'address street 123',
-    idNumber: '2363623',
-    profile: 'Construction Working',
-    displayInfo: function () {
-        console.log(this.name, '\n', this.address, '\n', this.idNumber, '\n', this.profile);
-    },
-    changeId: function (num) {
-        this.idNumber = num.toString()
-    }
-};
-
-// studentES5.displayInfo();
-
-// studentES5.changeId(333222);
-// studentES5.displayInfo();
-
-const studentES6 = {
-    name: 'Bandrzej Pajda',
-    address: 'Fiolkowa 123',
-    idNumber: '0308802210',
-    profile: 'Zawud Piekasz',
-    displayInfo: function () {
-        console.log(`${this.name}\n${this.address}\n${this.idNumber}\n${this.profile}`);
-    },
-    changeId: function (num) {
-        this.idNumber = num.toString()
-    }
-};
-
 const ultimateStudentES6 = {
     name: '',
     address: '',
@@ -52,12 +21,6 @@ properStudent.displayInfo();
 const properStudent2 = Object.create((ultimateStudentES6));
 properStudent2.setStudentData('Andrzej', 'Rockowa 555', 8850, 'Engineer');
 
-// const firstStudent = Object.create(ultimateStudentES6);
-// firstStudent.setName('Michal');
-// firstStudent.setAddress('Ulica 1');
-// firstStudent.setIdNumber(12323);
-// firstStudent.setProfile('Medical Student');
-// console.log(firstStudent);
 const properStudent3 = Object.create((ultimateStudentES6));
 
 
@@ -66,24 +29,31 @@ properStudent3.setStudentData('DzejDzej', 'Jazzowa 1358', 12323, 'SSS');
 const studentsList = [];
 
 
-var outputHtml = '';
+let outputHtml = '';
 studentsList.push(properStudent, properStudent2, properStudent3);
 console.log(studentsList);
 const htmlAnchor = document.querySelector('.student__container');
-//
-// studentsList.forEach((item) => {
-//    htmlAnchor
-// });
+
+function studentItemDiv(className, item, arg) {
+    return `<div class="${className}">${item[arg]}</div>`
+}
 
 studentsList.map((element) => {
-    outputHtml += `
-<div class="student__row">
-<div class="student__item">${element.name}</div>
-<div class="student__item">${element.address} </div>
-<div class="student__item">${element.idNumber} </div>
-<div class="student__item">${element.profile}</div>
+    htmlAnchor.innerHTML = outputHtml;
 
-</div>`;
-
-    htmlAnchor.innerHTML = outputHtml
+//     outputHtml += `
+// <div class="student__row">
+// <div class="student__item">${element.name}</div>
+// <div class="student__item">${element.address} </div>
+// <div class="student__item">${element.idNumber} </div>
+// <div class="student__item">${element.profile}</div>
+// </div>`;
+//
+    outputHtml +=
+        '<div class="student__row">' +
+        studentItemDiv('student__item', element, 'name') +
+        studentItemDiv('student__item', element, 'address') +
+        studentItemDiv('student__item', element, 'idNumber') +
+        studentItemDiv('student__item', element, 'profile') +
+        '</div>' ;
 });
