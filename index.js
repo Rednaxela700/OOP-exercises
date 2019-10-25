63978054dd1430fe0b77ffc79c27c29151acfe21
rednaxela700 <ohleg@windowslive.com>
2019-10-25 10:40:37 +0200

    added person obj changing account numbers to ****

--- index.js
+++ index.js
@@ -3,21 +3,34 @@
 //constructor name should start with capital letter
 class Person {
     constructor(fullName, accountNumber) {
-        this.fullName = fullName;
-        this.accountNumber = accountNumber;
+        this._fullName = fullName; //private methods
+        this._accountNumber = accountNumber; //private methods
     }
-
-    displayAccountNum() {
-
-        const num = this.accountNumber;
-        // for(let i = 3; i < num.length - 4; i++)
-        //     num[i] = '*'
-
-        console.log(num[3])
-
+    get fullName() {
+        return this._fullName
+    }
+    get accountNumber() {
+        return this._accountNumber.replace(
+            this._accountNumber.slice(2, 22), ' **** **** **** **** ****'
+        )
     }
 }
 
-const personExample = new Person('Andrzej Wajda', '82 8888 1111 8888 8888 8888 0000');
+
+const personExample = new Person('Andrzej Wajda', '82 8888 1111 8888 8888 0000');
 // console.log(personExample);
-personExample.displayAccountNum()
+// personExample.displayAccountNum();
+
+let str = '82 8888 1111 8888 8888  0000';
+// str.split(' ')
+anotherStr = str.split(' ').splice(1, 5)
+console.log(anotherStr);
+
+
+const peopleList = [
+    new Person('Ja 2', '4240 2301 0000 0010 0000 9371'),
+    new Person('Ja 3', '4240 1111 0100 0010 8888 9371'),
+    personExample
+];
+console.log(personExample.accountNumber)
+
