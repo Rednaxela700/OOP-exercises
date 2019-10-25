79086a8109d3ef4fb247cc2e44fe96016546b176
rednaxela700 <ohleg@windowslive.com>
2019-10-25 13:25:47 +0200

    moving div up, left. Even outside outer box

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
-
-class Render {
-    static renderList(listToRender, elementToRender) {
-        const element = document.querySelector(elementToRender);
-        listToRender.map((currentElement) => {
-            console.log(currentElement.fullName);
-            element.innerHTML += `
-<div class="student__row">
-<div class="student__item">${currentElement.fullName}</div>
-<div class="student__item">${currentElement.accountNumber}</div>
-</div>`
-        })
+const board = {
+  id: document.getElementById('board'),
+};
+const player = {
+    id: document.getElementById('player'),
+    moveLeft() {
+        console.log(this.id.offsetLeft);
+        console.log(this.id.offsetLeft);
+        this.id.style.left = '10px';
     }
+};
+
+
+console.log(player.id);
+board.id.addEventListener('click', () => {
+    let actualPosition = player.id.offsetLeft;
+    return player.id.style.left = `${(actualPosition + 10).toString()}px`
+});
+
+function getPosition(element) {
+
 }
 
-// Render.renderList(peopleList, '.student__container')
+document.body.addEventListener('keydown', function (event) {
+    const key = event.key;
+    let actualPositionX = player.id.offsetLeft;
+    let actualPositionY = player.id.offsetTop;
+    switch (key) {
+        case 'ArrowLeft':
+            console.log('lewo');
+            return player.id.style.left = `${(actualPositionX - 10).toString()}px`;
+            break;
+        case 'ArrowRight':
+            console.log('prawo');
+            return player.id.style.left = `${(actualPositionX + 10).toString()}px`;
+            break;
+        case 'ArrowUp':
+            console.log('góra');
+            return player.id.style.top = `${(actualPositionY - 10).toString()}px`;
+            break;
+        case 'ArrowDown':
+            console.log('dół');
+            return player.id.style.top = `${(actualPositionY + 10).toString()}px`;
+            break;
+    }})
