1669ca0ac28cc17e8ba83d6f9f7b3508e1e83ce5
rednaxela700 <ohleg@windowslive.com>
2019-10-25 14:07:30 +0200

    put movePlayer function as a player object method

--- index.js
+++ index.js
@@ -1,45 +1,29 @@
 const board = {
-  id: document.getElementById('board'),
+    id: document.getElementById('board'),
 };
 const player = {
     id: document.getElementById('player'),
-    moveLeft() {
-        console.log(this.id.offsetLeft);
-        console.log(this.id.offsetLeft);
-        this.id.style.left = '10px';
+    move(direction) {
+        let positionX = this.id.offsetLeft;
+        let positionY = this.id.offsetTop;
+        switch (direction) {
+            case 'ArrowLeft':
+                this.id.style.left = `${(positionX - 10).toString()}px`;
+                break;
+            case 'ArrowRight':
+                this.id.style.left = `${(positionX + 10).toString()}px`;
+                break;
+            case 'ArrowUp':
+                this.id.style.top = `${(positionY - 10).toString()}px`;
+                break;
+            case 'ArrowDown':
+                this.id.style.top = `${(positionY + 10).toString()}px`;
+                break;
+            default:
+                console.log('try with arrows, dumbooooo');
+        }
     }
 };
-
-
-console.log(player.id);
-board.id.addEventListener('click', () => {
-    let actualPosition = player.id.offsetLeft;
-    return player.id.style.left = `${(actualPosition + 10).toString()}px`
+document.body.addEventListener('keydown', function(event) {
+    player.move(event.key)
 });
-
-function getPosition(element) {
-
-}
-
-document.body.addEventListener('keydown', function (event) {
-    const key = event.key;
-    let actualPositionX = player.id.offsetLeft;
-    let actualPositionY = player.id.offsetTop;
-    switch (key) {
-        case 'ArrowLeft':
-            console.log('lewo');
-            return player.id.style.left = `${(actualPositionX - 10).toString()}px`;
-            break;
-        case 'ArrowRight':
-            console.log('prawo');
-            return player.id.style.left = `${(actualPositionX + 10).toString()}px`;
-            break;
-        case 'ArrowUp':
-            console.log('góra');
-            return player.id.style.top = `${(actualPositionY - 10).toString()}px`;
-            break;
-        case 'ArrowDown':
-            console.log('dół');
-            return player.id.style.top = `${(actualPositionY + 10).toString()}px`;
-            break;
-    }})
