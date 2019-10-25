5b472480f0aaa44a3a980ad99c7f82cc20dcbb07
rednaxela700 <ohleg@windowslive.com>
2019-10-25 11:14:11 +0200

    added a render class, changed people names

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ class Person {
         this._fullName = fullName; //private methods
         this._accountNumber = accountNumber; //private methods
     }
+
     get fullName() {
         return this._fullName
     }
+
     get accountNumber() {
         return this._accountNumber.replace(
             this._accountNumber.slice(2, 22), ' **** **** **** **** ****'
@@ -16,21 +18,29 @@ class Person {
     }
 }
 
-
-const personExample = new Person('Andrzej Wajda', '82 8888 1111 8888 8888 0000');
+const personExample = new Person('Beata Tyszkiewicz', '82 8888 1111 8888 8888 0000');
 // console.log(personExample);
 // personExample.displayAccountNum();
 
-let str = '82 8888 1111 8888 8888  0000';
-// str.split(' ')
-anotherStr = str.split(' ').splice(1, 5)
-console.log(anotherStr);
-
 
 const peopleList = [
-    new Person('Ja 2', '4240 2301 0000 0010 0000 9371'),
-    new Person('Ja 3', '4240 1111 0100 0010 8888 9371'),
+    new Person('Andrzej Wajda', '4240 2301 0000 0010 0000 9371'),
+    new Person('Zbigniew Wodecki', '4240 1111 0100 0010 8888 9371'),
     personExample
 ];
-console.log(personExample.accountNumber)
 
+class Render {
+    static renderList(listToRender, elementToRender) {
+        const element = document.querySelector(elementToRender);
+        listToRender.map((currentElement) => {
+            console.log(currentElement.fullName);
+            element.innerHTML += `
+<div class="student__row">
+<div class="student__item">${currentElement.fullName}</div>
+<div class="student__item">${currentElement.accountNumber}</div>
+</div>`
+        })
+    }
+}
+
+Render.renderList(peopleList, '.student__container')
