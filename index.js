926d12d00fbc9fc8cadce6a416a80dd6d3fa49cd
rednaxela700 <ohleg@windowslive.com>
2019-10-25 11:30:44 +0200

    game init - board is ready

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-//creating person class
-
-//constructor name should start with capital letter
-class Person {
-    constructor(fullName, accountNumber) {
-        this._fullName = fullName; //private methods
-        this._accountNumber = accountNumber; //private methods
-    }
-
-    get fullName() {
-        return this._fullName
-    }
-
-    get accountNumber() {
-        return this._accountNumber.replace(
-            this._accountNumber.slice(2, 22), ' **** **** **** **** ****'
-        )
-    }
-}
-
-const personExample = new Person('Beata Tyszkiewicz', '82 8888 1111 8888 8888 0000');
-// console.log(personExample);
-// personExample.displayAccountNum();
-
-
-const peopleList = [
-    new Person('Andrzej Wajda', '4240 2301 0000 0010 0000 9371'),
-    new Person('Zbigniew Wodecki', '4240 1111 0100 0010 8888 9371'),
-    personExample
-];
 
 class Render {
     static renderList(listToRender, elementToRender) {
@@ -43,4 +13,4 @@ class Render {
     }
 }
 
-Render.renderList(peopleList, '.student__container')
+// Render.renderList(peopleList, '.student__container')
