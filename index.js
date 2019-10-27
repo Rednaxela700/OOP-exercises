9e07d31a44253a3fdc827a46185a734374273ed4
rednaxela700 <ohleg@windowslive.com>
2019-10-27 19:34:41 +0100

    function collision, no moving out of left border

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const board = {
     id: document.getElementById('board'),
-    width: document.getElementById('board').offsetWidth
+    width: document.getElementById('board').offsetWidth,
     height: document.getElementById('board').offsetHeight
     // height: this.id.offsetHeight
 };
-console.log(board.id.offsetWidth)
+console.log(board.id.offsetWidth);
 const player = {
     id: document.getElementById('player'),
     move(direction) {
-        let positionX = this.id.offsetLeft;
-        let positionY = this.id.offsetTop;
+        const positionX = this.id.offsetLeft;
+        const positionY = this.id.offsetTop;
         switch (direction) {
             case 'ArrowLeft':
-                this.id.style.left = `${(positionX - 10).toString()}px`;
+                // this.id.style.left = `${(positionX - 10).toString()}px`;
+                collision()
                 break;
             case 'ArrowRight':
                 this.id.style.left = `${(positionX + 10).toString()}px`;
                 break;
             case 'ArrowUp':
                 this.id.style.top = `${(positionY - 10).toString()}px`;
+                // collision()
                 break;
             case 'ArrowDown':
                 this.id.style.top = `${(positionY + 10).toString()}px`;
@@ -28,6 +30,26 @@ const player = {
         }
     }
 };
-document.body.addEventListener('keydown', function(event) {
+document.body.addEventListener('keydown', function (event) {
     player.move(event.key)
 });
+
+function collision() {
+
+
+    const {id} = player;
+    const {height, width} = board;
+    const positionX = id.offsetLeft;
+    const positionY = id.offsetTop;
+    const maxPositionY = height;
+
+// && positionY < maxPositionY
+    if (positionX > 0 ) {
+        id.style.left = `${(positionX - 10).toString()}px`;
+    } else {
+        console.log(`first collision else log`);
+    }
+
+    console.log(height);
+    console.log(`offsetTop: ${id.offsetTop}`)
+}
