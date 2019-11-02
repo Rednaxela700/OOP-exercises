c784780a529282efa4d93f912d37fea3ff78896c
rednaxela700 <ohleg@windowslive.com>
2019-11-02 22:17:36 +0100

    update position method not fully working, set values only on call in console

--- index.js
+++ index.js
@@ -6,19 +6,32 @@ const board = {
 };
 const food = {
     id: '',
+    position: {
+      x: '', //this.id.offsetLeft
+      y: ''  //this.id.offsetLeft
+    },
     createEl() {
         const el = document.createElement('div');
         el.setAttribute('id','food'); //add id=food attribute
         el.classList.add('game__food'); //add css class
-        el.style.top = this.changePosition(0, board.height);    //randomize position on board
-        el.style.left = this.changePosition(0, board.width);
-        board.id.appendChild(el)
+        el.style.top = this.changePosition(0, board.height, 'y');    //randomize position on board
+        el.style.left = this.changePosition(0, board.width, 'x');
+        board.id.appendChild(el);
         return this.id = document.getElementById('food') // return elements id set to food
     },
-    changePosition(min,max) {
+    updatePosition(axis) {
+        if (axis === 'x') {
+            return this.position.x = this.id.offsetLeft;
+        } else {
+            return this.position.y = this.id.offsetTop;
+        }
+
+    },
+    changePosition(min,max, axis) {
         const roundedMax = Math.round(max / 10) * 10; //rounding to tenth
         const randomPosition = Math.floor(Math.random() * (roundedMax - min + 1) + min); //generating random position (max = board width)
         const roundedRandomPos = Math.round(randomPosition / 10) * 10; //rounding position to tenth ex. 492 to 490
+        (axis === 'x') ? this.updatePosition('x'): this.updatePosition('y');  //update object position
         return `${roundedRandomPos}px`
     }
 };
@@ -37,6 +50,9 @@ const player = {
                 // this.id.style.left = `${(positionX - 10).toString()}px`;
 
                 if (positionX > 0) {
+                    if(positionX === food.position.x && positionY === food.position.y) {
+                        console.log('EATEN!');
+                    }//check if food is being eaten
                     id.style.left = `${(positionX - 10).toString()}px`;
                 } else {
                     console.log(`east wall collision else log`);
